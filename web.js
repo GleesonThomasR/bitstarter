e9fc37246928cabd90b6fb4937c1ac7695306045
Thomas Gleeson <Gleeson.ThomasR@gmail.com>
2013-07-14 21:52:29 +0000

    Added web.js

--- web.js
+++ web.js
@@ -6,11 +6,6 @@ app.get('/', function(request, response) {
   response.send("1");
 });
 
-var fs = require('fs');
-console.log("ok");
-var content = fs.readFileSync('./bitstarter/index.html');
-console.log("Contents: " + content);
-
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
