aae410e227f1b0871c8d876cb8a4ad5e22846ad1
Thomas Gleeson <Gleeson.ThomasR@gmail.com>
2013-07-14 22:11:08 +0000

    Added web js

--- web.js
+++ web.js
@@ -3,8 +3,15 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send("1");
+  response.send("2");
 });
+
+
+var fs = require('fs');
+console.log("hey");
+var content = fs.readFileSync('https://github.com/GleesonThomasR/bitstarter/index.html');
+console.log("you" + content);
+
 
 
 var port = process.env.PORT || 5000;
