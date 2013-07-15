b3b3910a218c22db8e321ccb47f335956a8ff935
Thomas Gleeson <Gleeson.ThomasR@gmail.com>
2013-07-15 00:34:44 +0000

    Added web.js

--- web.js
+++ web.js
@@ -3,11 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send("4001");
+  response.send("Hello World from index.html");
 });
-
-var text = fs.readFileSync('index.html', 'utf8');
-console.log (text);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
