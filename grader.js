b86a7d18f753369fff5246ff7fed93c4c40ae1f9
Thomas Gleeson <Gleeson.ThomasR@gmail.com>
2013-07-16 21:30:40 +0000

    Added grader.js

--- grader.js
+++ grader.js
@@ -24,9 +24,21 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://serene-mesa-9728.herokuapp.com/";
+var restler = require('restler');
+var util = require('util');
+
+
+var callThis = function(result) {
+if (data instanceof Error) {
+    sys.puts('Error: ' + result.message);
+    this.retry(5000); // try again after 5 sec
+  } else {
+    sys.puts(result);
+  }
+};
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -37,6 +49,10 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+var assertUrlExists = function(val) {   
+    return val.toString();
+};
+
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
@@ -45,7 +61,8 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtml = function($, checksfile) {
+var checkHtmlFile = function(htmlfile, checksfile) {
+    $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -53,10 +70,6 @@ var checkHtml = function($, checksfile) {
         out[checks[ii]] = present;
     }
     return out;
-};
-
-var checkHtmlFile = function(htmlfile, checksfile) {
-    return checkHtml(cheerioHtmlFile(htmlfile), checksfile);
 };
 
 var clone = function(fn) {
@@ -69,19 +82,11 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url [url]', 'URL to check')
-        .parse(process.argv);
-    if (program.url) {
-        rest.get(program.url).on('complete', function(result) {
-            var checkJson = checkHtml(cheerio.load(result), program.checks);
-            var outJson = JSON.stringify(checkJson, null, 4);
-            console.log(outJson);
-        });
-    } else {
-        var checkJson = checkHtmlFile(program.file, program.checks);
-        var outJson = JSON.stringify(checkJson, null, 4);
-        console.log(outJson);
-    }
+        .option('-u, --url <url_address>', 'Path to an internet Address', clone(assertUrlExists), URL_DEFAULT)
+	.parse(process.argv);
+    var checkJson = checkHtmlFile(program.file, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
